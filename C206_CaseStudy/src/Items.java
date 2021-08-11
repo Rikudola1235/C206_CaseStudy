d2d4f81fa5fca68bbe5dbf01ed1c153619996e47
20021085 <20021085@Joseph-PC>
2021-08-11 16:22:22 +0800

    update Items

---
+++ C206_CaseStudy/src/Items.java
@@ -0,0 +1,12 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 20021085, 11 Aug 2021 4:18:36 pm
+ */
+
+public class Items {
+
+}
