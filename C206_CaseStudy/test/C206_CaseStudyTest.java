ae37e94a23b9d30f14e9d4f1d6c5ffd62ac4a65f
20014059 <20014059@DESKTOP-B6IHQ1T.lan>
2021-08-15 10:31:29 +0800

    update test cases

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -153,6 +153,43 @@ public class C206_CaseStudyTest {
 	}
 	
 	@Test
+	public void testViewMonthlyMenu() {
+		// Balqis' testViewMonthlyMenu
+		// Monthly Menu list is not null, so that can add a new item
+		assertNotNull("Test if there is valid monthly menu arraylist to add to", monthlyList);
+		
+		// test if the list of Monthly Menu retrieved from the main class is empty
+		String monthlyMenu = C206_CaseStudy.retrieveMonthlyMenu(monthlyList);
+		String testOutput = "";
+
+		assertEquals("Check that viewMonthlyMenu", testOutput, monthlyMenu);
+				
+		// Given an empty list, after adding 1 item, the size of the list is 1
+		C206_CaseStudy.addMonthlyMenu(monthlyList, mMenu1);
+		assertEquals("Check of account list size is 1", monthlyList.size(), 1);
+
+		// The newly Monthly Menu added is as same as the first item of the list
+		assertSame("Test that Monthly Menu added is same as 1st item of the list?", mMenu1, monthlyList.get(0));
+				
+		// Add another Monthly Menu, test the size of the list is 2?
+		C206_CaseStudy.addMonthlyMenu(monthlyList, mMenu2);
+		assertEquals("Check of account list size is 2", monthlyList.size(), 2);
+
+		// The newly Monthly Menu added is as same as the second item of the list
+		assertSame("Test that Monthly Menu added is same as 2nd item of the list?", mMenu2, monthlyList.get(1));
+		
+
+		// test if the expected output is same as the list of monthly menu retrieved from the main class
+		monthlyMenu = C206_CaseStudy.retrieveMonthlyMenu(monthlyList);
+
+		testOutput = String.format("%-5d %-20s %-20s %-20s\n", 1, "Asian", "Laksa", "Iced Tea");
+		testOutput+= String.format("%-5d %-20s %-20s %-20s\n", 2, "Asian", "Wanton Noodle", "Iced Coffee");
+
+		assertEquals("Check that viewMonthlyMenu", testOutput, monthlyMenu);
+		
+	}
+	
+	@Test
 	public void testAddMonthlyMenu() {
 		// Balqis' testGenerateMonthlyMenu
 		// Monthly Menu list is not null, so that can generate a new Monthly Menu
@@ -160,35 +197,35 @@ public class C206_CaseStudyTest {
 		
 		// Given an empty list, after adding 1 item, the size of the list is 1
 		C206_CaseStudy.addMonthlyMenu(monthlyList, mMenu1);
-		assertEquals("Check of account list size is 1", accountList.size(), 1);
+		assertEquals("Check of account list size is 1", monthlyList.size(), 1);
 
 		// The newly Monthly Menu added is as same as the first item of the list
 		assertSame("Test that Monthly Menu added is same as 1st item of the list?", mMenu1, monthlyList.get(0));
 
 		// Add another Monthly Menu, test the size of the list is 2?
 		C206_CaseStudy.addMonthlyMenu(monthlyList, mMenu2);
-		assertEquals("Check of account list size is 2", accountList.size(), 2);
+		assertEquals("Check of account list size is 2", monthlyList.size(), 2);
 
 		// The newly Monthly Menu added is as same as the second item of the list
 		assertSame("Test that Monthly Menu added is same as 2nd item of the list?", mMenu2, monthlyList.get(1));
 		
 		// Add another Monthly Menu, test the size of the list is 3?
 		C206_CaseStudy.addMonthlyMenu(monthlyList, mMenu3);
-		assertEquals("Check of account list size is 3", accountList.size(), 3);
+		assertEquals("Check of account list size is 3", monthlyList.size(), 3);
 
 		// The newly Monthly Menu added is as same as the third item of the list
 		assertSame("Test that Monthly Menu added is same as 3rd item of the list?", mMenu3, monthlyList.get(2));
 		
 		// Add another Monthly Menu, test the size of the list is 4?
 		C206_CaseStudy.addMonthlyMenu(monthlyList, mMenu4);
-		assertEquals("Check of account list size is 4", accountList.size(), 4);
+		assertEquals("Check of account list size is 4", monthlyList.size(), 4);
 
 		// The newly Monthly Menu added is as same as the second item of the list
 		assertSame("Test that Monthly Menu added is same as 4th item of the list?", mMenu4, monthlyList.get(3));
 		
 		// Add another Monthly Menu, test the size of the list is 5?
 		C206_CaseStudy.addMonthlyMenu(monthlyList, mMenu5);
-		assertEquals("Check of account list size is 5", accountList.size(), 5);
+		assertEquals("Check of account list size is 5", monthlyList.size(), 5);
 
 		// The newly Monthly Menu added is as same as the second item of the list
 		assertSame("Test that Monthly Menu added is same as 5th item of the list?", mMenu5, monthlyList.get(4));
@@ -208,14 +245,14 @@ public class C206_CaseStudyTest {
 		
 		// Given an empty list, after adding 1 item, the size of the list is 1
 		C206_CaseStudy.addMonthlyMenu(monthlyList, mMenu1);
-		assertEquals("Check of account list size is 1", accountList.size(), 1);
+		assertEquals("Check of account list size is 1", monthlyList.size(), 1);
 
 		// The newly Monthly Menu added is as same as the first item of the list
 		assertSame("Test that Monthly Menu added is same as 1st item of the list?", mMenu1, monthlyList.get(0));
 		
 		// Add another Monthly Menu, test the size of the list is 2?
 		C206_CaseStudy.addMonthlyMenu(monthlyList, mMenu2);
-		assertEquals("Check of account list size is 2", accountList.size(), 2);
+		assertEquals("Check of account list size is 2", monthlyList.size(), 2);
 
 		// The newly Monthly Menu added is as same as the second item of the list
 		assertSame("Test that Monthly Menu added is same as 2nd item of the list?", mMenu2, monthlyList.get(1));
@@ -230,42 +267,7 @@ public class C206_CaseStudyTest {
 
 	}
 	
-	@Test
-	public void testViewMonthlyMenu() {
-		// Balqis' testViewMonthlyMenu
-		// Monthly Menu list is not null, so that can add a new item
-		assertNotNull("Test if there is valid monthly menu arraylist to add to", monthlyList);
-		
-		// test if the list of Monthly Menu retrieved from the main class is empty
-		String monthlyMenu = C206_CaseStudy.retrieveMonthlyMenu(monthlyList);
-		String testOutput = "";
-
-		assertEquals("Check that viewMonthlyMenu", testOutput, monthlyMenu);
-				
-		// Given an empty list, after adding 1 item, the size of the list is 1
-		C206_CaseStudy.addMonthlyMenu(monthlyList, mMenu1);
-		assertEquals("Check of account list size is 1", accountList.size(), 1);
-
-		// The newly Monthly Menu added is as same as the first item of the list
-		assertSame("Test that Monthly Menu added is same as 1st item of the list?", mMenu1, monthlyList.get(0));
-				
-		// Add another Monthly Menu, test the size of the list is 2?
-		C206_CaseStudy.addMonthlyMenu(monthlyList, mMenu2);
-		assertEquals("Check of account list size is 2", accountList.size(), 2);
-
-		// The newly Monthly Menu added is as same as the second item of the list
-		assertSame("Test that Monthly Menu added is same as 2nd item of the list?", mMenu2, monthlyList.get(1));
-		
-
-		// test if the expected output is same as the list of monthly menu retrieved from the main class
-		monthlyMenu = C206_CaseStudy.retrieveMonthlyMenu(monthlyList);
-
-		testOutput = String.format("%-5d %-20s %-20s %-20s\n", 1, "Asian", "Laksa", "Iced Tea");
-		testOutput+= String.format("%-5d %-20s %-20s %-20s\n", 2, "Asian", "Wanton Noodle", "Iced Coffee");
-
-		assertEquals("Check that viewMonthlyMenu", testOutput, monthlyMenu);
-		
-	}
+	
 
 	@After
 	public void tearDown() throws Exception {
